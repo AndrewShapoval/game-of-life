488a10fdf9284ae7d14cd94f36fbcf4a55c43f71
AndrewShapoval <andrewshapoval88@gmail.com>
2020-11-18 21:04:44 +0300

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
 
-function App() {
+export const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <h1>Game of Life</h1>
     </div>
   );
 }
-
-export default App;
