c0f35224afab0fe6ff1e539e3d963ae5a9218ac4
AndrewShapoval <andrewshapoval88@gmail.com>
2020-11-19 23:07:57 +0300

    added a grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './App.module.scss';
+import produce from "immer"
 
 export const App: React.FC = () => {
-  return (
-    <div className={styles.app}>
-      <h1>Game of Life</h1>
-    </div>
-  );
+
+    const numRows = 37
+    const numCols = 50
+
+    const [grid, setGrid] = useState(() => {
+        const rows = []
+        for (let i = 0; i < numRows; i++) {
+            rows.push(Array.from(Array(numCols), () => 0))
+        }
+        return rows
+    })
+    console.log(grid)
+
+    return (
+        <div className={styles.app}>
+            <h1>Game of Life</h1>
+            <div className={styles.container}>
+                {grid.map((rows, i) =>
+                    <div key={i} className={styles.rows}>
+                        {rows.map((col, k) =>
+                            <div key={`${i}-${k}`} className={grid[i][k] ? styles.busyCell : styles.cell}
+                                 onClick={() => {
+                                     const newGrid = produce(grid, gridCopy => {
+                                         gridCopy[i][k] = gridCopy[i][k] ? 0 : 1
+                                     })
+                                     setGrid(newGrid)
+                                 }}/>
+                        )}</div>
+                )}</div>
+        </div>
+    );
 }
